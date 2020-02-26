aca931a86e9f6d88cd65e0863ebd2ebb928b4419
Luis Carlin <luiscarlin@gmail.com>
2020-02-26 18:01:34 -0500

    remove root route

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -3,10 +3,6 @@ import productsRouter from './routes/products'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('An alligator approaches!')
-})
-
 app.use('/api/products', productsRouter)
 
 app.listen(5000, () => console.log('Gator app listening on port 5000!'))
